21579835a9b45da73efaad6385546689ac133129
Jacob G <jacob-x@jvvgindustries.com>
2016-02-10 21:25:11 -0500

    added uninstaller

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,16 @@
 <?php
+ExtensionConfig::remove_language_key('imageuploading');
+ExtensionConfig::remove-page('/extensions/uploadimage');
+ExtensionConfig::remove_page('/admin/imageuploading', array('file' => 'admin/imageuploading.php', 'template' => true, 'nocontentbox' => true, 'admin' => true));
+ExtensionConfig::remove_admin_menu('imageuploading');
+ExtensionConfig::remove_page('/myimages');
+
+$db->query('DELETE FROM `#^config` WHERE c_name IN(\'user_image_maxwidth\', \'user_image_maxheight\', \'user_image_maxsize\')') or enhanced_error('Failed to delete config entries', true);
+
+$db->drop_table('userimages');
+
+$db->drop_field('user_groups', 'g_upload_images');
+
+unlink(FORUM_ROOT . '/app_resources/pages/myimages.php');
+unlink(FORUM_ROOT . '/app_resources/pages/uploadimage.php');
+unlink(FORUM_ROOT . '/app_resources/pages/admin/imageuploading.php');
